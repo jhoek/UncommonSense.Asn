839ae58c5794432253f25be6ad347ab015d1f71d
Jan Hoek <jhoek@uncommonsense.nl>
2021-07-06 19:51:45 +0200

    Added README, PREFACE and script to update README

---
+++ README.md
@@ -0,0 +1,17 @@
+# UncommonSense.Asn
+
+PowerShell module for ASN Bank investments funds
+
+## Dependencies
+- Relies on [pup](https://github.com/ericchiang/pup) to parse HTML.
+
+<a name="Get-AsnFundPrice"></a>
+## Get-AsnFundPrice
+### Synopsis
+Get-AsnFundPrice
+### Syntax
+```powershell
+Get-AsnFundPrice
+```
+### Parameters
+<div style='font-size:small; color: #ccc'>Generated 06-07-2021 19:51</div>
---
+++ Update-README.ps1
@@ -0,0 +1,8 @@
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath UncommonSense.Asn.psd1) -Force
+
+Get-Command -Module UncommonSense.Asn |
+    Convert-HelpToMarkDown `
+        -Title 'UncommonSense.Asn' `
+        -Preface PREFACE.md `
+        -Description 'PowerShell module for ASN Bank investments funds' |
+    Out-File -FilePath (Join-Path -Path $PSScriptRoot -ChildPath README.md) -Encoding utf8
