dafca251a0c9c9a0f3c4460e796622365e991639
Jan Hoek <jhoek@uncommonsense.nl>
2021-07-06 19:26:23 +0200

    Finished function

--- UncommonSense.Asn.psm1
+++ UncommonSense.Asn.psm1
@@ -1,36 +1,3 @@
-<#
-#!/usr/bin/pwsh
-function Format-Value($Date, $Value, $PreviousValue)
-{
-    switch ($true)
-    {
-        ($PreviousValue -eq 0 -or $Value -eq $PreviousValue) { return "$($Date): &euro;$Value" }
-        ($Value -gt $PreviousValue) { return "$($Date): <font color='#090'>&euro;$Value &#x25B2; &euro; $($Value - $PreviousValue)</font>"; break }
-        ($Value -lt $PreviousValue) { return "$($Date): <font color='#900'>&euro;$Value &#x25BC; &euro; $($Value - $PreviousValue)</font>" }
-    }
-}
-
-$Content = Invoke-WebRequest -Uri 'https://www.asnbank.nl/beleggen/koersen.html' | Select-Object -ExpandProperty Content
-$Headings = $Content | pup '.fundrates thead tr text{}' --plain | ForEach-Object { $_.Trim() } | Where-Object { $_ }
-$Row = $Content | pup '.fundrates tr:nth-of-type(4) text{}' --plain | ForEach-Object { $_.Trim() } | Where-Object { $_ }
-
-$Dates = $Headings[1..3]
-$Rates = $Row[1..3] | ForEach-Object { [decimal]::Parse($_, [cultureinfo]::GetCultureInfo('nl-NL')) }
-$FundName = $Row[0]
-
-$Message = 0..1 |
-    ForEach-Object `
-        -Process { Format-Value $Dates[$_] $Rates[$_] $Rates[$_ + 1] }`
-        -End { Format-Value $Dates[2] $Rates[2] 0 }
-
-Send-PushoverNotification `
-    -ApplicationToken a635f2bxdynw7z839eb49o7dn1312c `
-    -Recipient u65ckN1X5uHueh7abnWukQ2owNdhAp `
-    -Title $FundName `
-    -Message ($Message -join "`n") `
-    -Html
- #>
-
 function Get-AsnFundPrice
 {
     param
@@ -38,47 +5,59 @@ function Get-AsnFundPrice
     )
 
     $DutchCulture = [cultureinfo]::new('nl-NL')
-    $CurrentFundProperties = [Ordered]@{ }
 
     $Content = Invoke-WebRequest https://www.asnbank.nl/beleggen/koersen.html `
     | Select-Object -ExpandProperty Content `
     | hxnormalize -x -l 1000 -i 0
 
-    $PriceDate = $Content `
-    | hxselect -c -s '\n' 'h3:nth-of-type(1)' `
-    | Select-String -Pattern '^Rendementen per .*? en koersen per (.*)$' `
-    | Select-Object -ExpandProperty Matches `
-    | Select-Object -ExpandProperty Groups `
-    | Select-Object -Skip 1 `
-    | Select-Object -ExpandProperty Value `
-    | ForEach-Object { [DateTime]::ParseExact($_, 'd-M-yy', $DutchCulture) }
+    $Dates = $Content
+    | pup '.fundrates thead tr text{}' --plain
+    | ForEach-Object { $_.Trim() }
+    | Where-Object { $_ }
+    | Select-Object -Skip 1
+    | ForEach-Object { [DateTime]::ParseExact($_, 'dd-MM-yyyy', $DutchCulture) }
 
-    $Content | hxextract table - `
-    | hxremove -i 'tr:nth-of-type(1)' `
-    | hxremove -i 'td:nth-of-type(3)' `
-    | hxremove -i 'td:nth-of-type(3)' `
-    | hxremove -i 'td:nth-of-type(3)' `
-    | hxremove -i 'td:nth-of-type(3)' `
-    | hxremove -i 'td:nth-of-type(3)' `
-    | xml2 `
-    | Where-Object { $_ -like '/table/tbody/tr/td/p*' } `
-    | ForEach-Object {
-        switch -regex ($_)
+    $Cells = $Content
+    | pup '.fundrates tbody tr td text{}' --plain
+
+    0..($Cells.Length - 1) | ForEach-Object {
+        $CurrentIndex = $_
+
+        switch ($true)
         {
-            '^/table/tbody/tr/td/p/b=(.*)\s*$'
+            ($CurrentIndex % 4 -eq 0)
             {
+                $CurrentFundProperties = [Ordered]@{}
                 $CurrentFundProperties.PSTypeName = 'UncommonSense.Asn.FundPrice'
-                $CurrentFundProperties.Date = $PriceDate
-                $CurrentFundProperties.Fund = $Matches[1]
+                $CurrentFundProperties.Fund = $Cells[$CurrentIndex]
+                break
             }
 
-            '/table/tbody/tr/td/p=€ (.*)\s*$'
+            ($CurrentIndex % 4 -eq 1)
             {
-                $CurrentFundProperties.Price = [decimal]::Parse($Matches[1], $DutchCulture)
+                $CurrentFundProperties.Date = $Dates[0]
+                $CurrentFundProperties.Price = [decimal]::Parse($Cells[$CurrentIndex], $DutchCulture)
                 [PSCustomObject]$CurrentFundProperties
+                break
+            }
 
-                $CurrentFundProperties = [Ordered]@{ }
+            ($CurrentIndex % 4 -eq 2)
+            {
+                $CurrentFundProperties.Date = $Dates[1]
+                $CurrentFundProperties.Price = [decimal]::Parse($Cells[$CurrentIndex], $DutchCulture)
+                [PSCustomObject]$CurrentFundProperties
+                break
+            }
+
+            ($CurrentIndex % 4 -eq 3)
+            {
+                $CurrentFundProperties.Date = $Dates[2]
+                $CurrentFundProperties.Price = [decimal]::Parse($Cells[$CurrentIndex], $DutchCulture)
+                [PSCustomObject]$CurrentFundProperties
+                break
             }
         }
     }
 }
+
+Get-AsnFundPrice
